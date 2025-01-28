fce9a5b0bd866ce9e5eb94bcd5022f6fd4e36674
Suzume Nomura <SuzuME@ea.g1e.org>
2025-01-28 18:29:39 +0900

    READMEにopenapi-generatorの出力を追加

--- scripts/put_readme.sh
+++ scripts/put_readme.sh
@@ -1,4 +1,11 @@
 #!/bin/sh -ex
 
 API_VERSION=`jq .info.version < spec/spec.json`
+cp README.md README.md.swp
 awk "{ gsub(\": 不明\",\": ${API_VERSION:-不明}\"); print }" < README.tpl > README.md
+{
+  echo
+  echo
+  cat README.md.swp
+}  >> README.md
+rm README.md.swp 
